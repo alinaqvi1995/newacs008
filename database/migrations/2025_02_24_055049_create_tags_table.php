1ae1f33a70c0f6fe20aebe94edac5c737c08f3e7
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-11 03:13:44 +0500

    s

--- database/migrations/2025_02_24_055049_create_tags_table.php
+++ database/migrations/2025_02_24_055049_create_tags_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
