b00d6518808590470939346683b890c15cafd1f7
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-12 18:12:38 +0500

    s

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -126,26 +126,26 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
-            $(".quantity-box").each(function() {
-                let $quantityInput = $(this).find(".quantity-input");
+            // $(".quantity-box").each(function() {
+            //     let $quantityInput = $(this).find(".quantity-input");
 
-                $(this).find(".add").click(function() {
-                    let currentVal = parseInt($quantityInput.val());
-                    let maxVal = parseInt($quantityInput.attr("max"));
+            //     $(this).find(".add").click(function() {
+            //         let currentVal = parseInt($quantityInput.val());
+            //         let maxVal = parseInt($quantityInput.attr("max"));
 
-                    if (currentVal < maxVal) {
-                        $quantityInput.val(currentVal + 1);
-                    }
-                });
+            //         if (currentVal < maxVal) {
+            //             $quantityInput.val(currentVal + 1);
+            //         }
+            //     });
 
-                $(this).find(".sub").click(function() {
-                    let currentVal = parseInt($quantityInput.val());
+            //     $(this).find(".sub").click(function() {
+            //         let currentVal = parseInt($quantityInput.val());
 
-                    if (currentVal > 1) {
-                        $quantityInput.val(currentVal - 1);
-                    }
-                });
-            });
+            //         if (currentVal > 1) {
+            //             $quantityInput.val(currentVal - 1);
+            //         }
+            //     });
+            // });
 
             $(".add-to-cart").click(function(e) {
                 e.preventDefault();
