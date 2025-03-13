d861cbd7f3f87a258d486b341eca52d7af44f0aa
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-03-13 17:33:08 +0500

    s

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Wishlist;
 use App\Models\User;
+use App\Models\Order;
 
 class DashboardController extends Controller
 {
