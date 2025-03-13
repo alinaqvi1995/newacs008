6bbd998b29d5994570b82bcc7c9f7661f63d7b7d
Haris Aftab <100057357+Harrisaftab@users.noreply.github.com>
2025-03-13 22:49:24 +0500

    shift subscribe to partials and include dynamic subscribe

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -22,31 +22,6 @@
                                             {{ $item->name }}<span class="fas fa-angle-right"></span></a>
                                     </li>
                                 @endforeach
-                                {{-- <li class="active"><a href="{{ route('frontend.products') }}"><i class="icon-fruit"></i>
-                                        Fresh
-                                        Fruits<span class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-salad-1"></i> Salads<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-meat"></i> Fresh Meat<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-eggs"></i> Butter & Egg<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-milk-box"></i> Milk Cream<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-vase"></i> Oil & Vinegers<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-french-toast"></i> Bread &
-                                        Bakery<span class="fas fa-angle-right"></span></a>
-                                </li>
-                                <li><a href="{{ route('frontend.products') }}"><i class="icon-corn"></i> Organic<span
-                                            class="fas fa-angle-right"></span></a>
-                                </li> --}}
                             </ul>
                         </div>
                     </div>
@@ -1000,57 +975,5 @@
             </div>
         </div>
     </section>
-    <section class="subscribe-one">
-        <div class="container">
-            <div class="subscribe-one__inner">
-                <div class="subscribe-one__shape-1 float-bob-x">
-                    <img src="frontend/assets/images/shapes/subscribe-one-shape-1.png" alt="">
-                </div>
-                <div class="subscribe-one__shape-2 float-bob-y">
-                    <img src="frontend/assets/images/shapes/subscribe-one-shape-2.png" alt="">
-                </div>
-                <div class="subscribe-one__shape-4 float-bob-y">
-                    <img src="frontend/assets/images/shapes/subscribe-one-shape-4.png" alt="">
-                </div>
-                <div class="subscribe-one__shape-5 zoominout">
-                    <img src="frontend/assets/images/shapes/subscribe-one-shape-5.png" alt="">
-                </div>
-                <div class="subscribe-one__inner-content">
-                    <div class="subscribe-one__shape-3 float-bob-x">
-                        <img src="frontend/assets/images/shapes/subscribe-one-shape-3.png" alt="">
-                    </div>
-                    <div class="row">
-                        <div class="col-xl-6">
-                            <div class="subscribe-one__left">
-                                <div class="icon">
-                                    <span class="icon-folder"></span>
-                                </div>
-                                <div class="subscribe-one__title-box">
-                                    <span class="subscribe-one__tagline">Quisque vel ortor</span>
-                                    <h2 class="subscribe-one__title">Subscribe to newsletter</h2>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-6">
-                            <div class="subscribe-one__right">
-                                <div class="subscribe-one__shape-6 float-bob-x">
-                                    <img src="frontend/assets/images/shapes/subscribe-one-shape-6.png" alt="">
-                                </div>
-                                <div class="subscribe-one__form-box">
-                                    <form class="subscribe-one__form mc-form" data-url="MC_FORM_URL">
-                                        <div class="subscribe-one__input-box">
-                                            <input type="email" placeholder="Email Address" id="subscriberEmail" name="email">
-                                            <button type="submit" class="subscribe-one__btn"><i
-                                                    class="fas fa-paper-plane"></i></button>
-                                        </div>
-                                    </form>
-                                    <div class="mc-form__response"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.partials.subscribe')
 @endsection
